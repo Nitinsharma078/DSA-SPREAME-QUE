028b8b38a03370b1e3724795a28f8e709de0b7e5
Nitinsharma078 <96969635+Nitinsharma078@users.noreply.github.com>
2023-04-07 16:19:27 +0530

    Add files via upload

--- subsenquences of string.cpp
+++ subsenquences of string.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void subsequence(string input,string output,int i){
+void subsequence(string input,string output,int i,vector<string>& v){
      //int n = input.size();
      if (i >=input.length())
      {
-          cout<<output<<endl;
+          //cout<<output<<endl;
+          v.push_back(output);
           return; 
      }
-     subsequence(input,output,i+1);
+     subsequence(input,output,i+1,v);
      output.push_back(input[i]);
-     subsequence(input,output,i+1);
+     subsequence(input,output,i+1,v);
      
 }
 int main(){
-     string input = "ab";
+     string input = "abc";
      string output = "";
+     vector<string>v;
      int i=0;
-     subsequence(input,output,i);
+     subsequence(input,output,i,v);
+     for(auto val:v){
+          cout<<val<<" "<<endl;
+
+     }
+     cout<<"size of string->"<<v.size();
      return 0;
 }
